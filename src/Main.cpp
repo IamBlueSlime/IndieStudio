b3e3261e7e83b42419ad06890eee830ddcc14a01
HECTOR Guillaume <LeChatErrant@guigui.home>
2019-05-17 02:34:53 +0200

    feat: entities now own a std::tuple of std::optional, not just a std::tuple of regular type  - Note : This code NEEDS to be reviewed with the rest of the group, because using a very tricky syntax on variadic arguments

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,10 +8,14 @@
 #include "ecs/Ecs.hpp"
 #include "indiestudio/Bootstraper.hpp"
 
+struct Component1;
+struct Component2;
+struct Component3;
+
 int main(int ac, char **av)
 {
-    auto manager = Ecs::Manager<int, float, std::string>();
-    manager.forEntitiesWith<int, std::string>(
+    auto manager = Ecs::Manager<Component1, Component2, Component3>();
+    manager.forEntitiesWith<Component1, Component3>(
         [](auto &data, auto id) {
             std::cout << "He! :D I found " << id << std::endl;
         });
--- src/ecs/Ecs.hpp
+++ src/ecs/Ecs.hpp
@@ -7,6 +7,18 @@
 #include <type_traits>
 #include <iostream>
 
+struct Component1 {
+    int x;
+    int y;
+};
+
+struct Component2 {
+    std::string str;
+};
+
+struct Component3 {
+};
+
 namespace Ecs
 {
 
@@ -55,7 +67,7 @@ namespace Ecs
         std::size_t id;
         std::optional<std::size_t> parent;
         std::bitset<sizeof...(components_types)> component_signature;
-        std::tuple<components_types...> components;     //TODO: faire un tuple d optionnal (HARDCORE)
+        std::tuple<std::optional<components_types>...> components;     //TODO: faire un tuple d optionnal (HARDCORE)
     };
 
     template<typename... components_types>
@@ -66,15 +78,41 @@ namespace Ecs
     public:
         Manager() {
             EntityType test1;
-            test1.component_signature[0] = true;
+            if (std::get<0>(test1.components) == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+
+            Component1 first;
+            Component2 second;
+            Component3 third;
+            std::get<0>(test1.components) = std::make_optional(first);
+
+            // Testing possibilty to pass variable to std::get at compile time using constexpr...
+            // Seems to be fucking difficult... That was not expected. Currently working on this
+            //
+            // std::cout << "Idx du comp 1: " << Components::template getIndex<Component1>() << std::endl;
+            // constexpr int idx = Components::template getIndex<Component1>();
+            // std::get<idx>(test1.components) = std::make_optional(first);
+            recomputeBitset(test1);
+            //test1.component_signature[0] = true;
 
             EntityType test2;
-            test2.component_signature[2] = true;
+            std::get<0>(test2.components) = std::make_optional(first);
+            std::get<1>(test2.components) = std::make_optional(second);
+            recomputeBitset(test2);
+            //test2.component_signature[0] = true;
+            //test2.component_signature[1] = true;
 
             EntityType test3;
-            test3.component_signature[0] = true;
-            test3.component_signature[1] = true;
-            test3.component_signature[2] = true;
+            std::get<0>(test3.components) = std::make_optional(first);
+            std::get<1>(test3.components) = std::make_optional(second);
+            std::get<2>(test3.components) = std::make_optional(third);
+            recomputeBitset(test3);
+            //test3.component_signature[0] = true;
+            //test3.component_signature[1] = true;
+            //test3.component_signature[2] = true;
 
             this->entities[0] = test1;
             this->entities[1] = test2;
@@ -106,8 +144,21 @@ namespace Ecs
         }
 
         // TODO: Pour recalculer le bitset lors de l'ajout / deletion d'un component à une entitée
-        void recomputeBitset(EntityType entity) {
-            // recalculate the component signature of an entity
+        void recomputeBitset(EntityType &entity) {
+            // Components::forEach([this, &entity](auto component, int idx) {
+            //     if (this->getComponent<typeof(component)>(entity) == std::nullopt) {
+            //         entity.component_signature[idx] = false;
+            //     } else {
+            //         entity.component_signature[idx] = true;
+            //     }
+            // });
+            // std::cout << "recomputed bitmap : " << entity.component_signature << std::endl;
+        }
+
+        // TODO: patch this function; actually, std::get can't take runtime variables :()
+        template<typename T>
+        std::optional<T> getComponent(const EntityType &entity) {
+            return std::get<Components::template getIndex<T>()>(entity.components);
         }
 
         // TODO: définir une manière d'ajouter des entitées
@@ -132,6 +183,6 @@ namespace Ecs
 
     protected:
     private:
-        std::map<std::size_t, EntityType> entities;
+        std::map<std::size_t, EntityType> entities; //TODO entity en unique ptr! Possibilité d'en ajouter / retirer oklm
     };
 }
