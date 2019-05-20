27eeedb597aa1bfec6eb35b0452ff959943c522e
HECTOR Guillaume <LeChatErrant@guigui.home>
2019-05-20 02:00:20 +0200

    abort: unsuccessful attempt to access tuple parts at runtime misc: code archived for later attempts

--- src/ecs/Ecs.hpp
+++ src/ecs/Ecs.hpp
@@ -6,6 +6,8 @@
 #include <functional>
 #include <type_traits>
 #include <iostream>
+#include <variant>
+#include <cassert>
 
 struct Component1 {
     int x;
@@ -42,7 +44,8 @@ namespace Ecs
 
         template<typename ToSearch, int idx>
         static constexpr int getIndex() {
-//            static_assert(idx != idx, "The type you are searching for is not in the TypeList! Are you using systems without appropriate components?\n")
+            std::cerr << "The type you are searching for is not in the TypeList! Are you using systems without appropriate components?\n" << std::endl;
+            assert(false);
             return -1;
         }
 
@@ -60,7 +63,108 @@ namespace Ecs
         template<typename Callable, int idx>
         static constexpr void forEach(Callable func) {
         }
+
     };
+
+    template<typename... components_types>
+    class Tuple {
+        using Components = TypeList<components_types...>;
+
+    public:
+        std::tuple<std::optional<components_types>...> components;
+
+        template <typename T>
+        std::optional<T> get() {
+            int idx = Components::template getIndex<T>();
+            return std::get<std::optional<T>>(Tuple::visit_at(components, idx));
+        }
+
+        template <typename T>
+        void set(T value) {
+            int idx = Components::template getIndex<T>();
+            return Tuple::replace_at(components, idx, std::make_optional(value));
+        }
+
+        template <typename T>
+        void unset() {
+            int idx = Components::template getIndex<T>();
+            return Tuple::replace_at(components, idx, std::nullopt);
+        }
+
+        template <int I>
+        struct visit_impl
+        {
+            template <typename T>
+            static std::variant<std::nullopt_t, std::optional<components_types>...> visit(T& tup, int idx)
+            {
+                if (idx == I - 1) {
+                    auto tmp = std::get<I - 1>(tup);
+                    return tmp;
+                } else {
+                    return visit_impl<I - 1>::visit(tup, idx);
+                }
+            }
+        };
+
+        template <>
+        struct visit_impl<0>
+        {
+            template <typename T>
+            static std::variant<std::nullopt_t, std::optional<components_types>...> visit(T& tup, int idx) {
+                std::cout << "The type you are searching is not in the tuple :/ how is it even possible?" << std::endl;
+                assert(false);
+                return std::nullopt;
+            }
+        };
+
+        template <typename... Ts>
+        std::variant<std::nullopt_t, std::optional<components_types>...> visit_at(std::tuple<Ts...> const& tup, int idx)
+        {
+            return visit_impl<sizeof...(Ts)>::visit(tup, idx);
+        }
+
+        template <typename... Ts>
+        std::variant<std::nullopt_t, std::optional<components_types>...> visit_at(std::tuple<Ts...>& tup, int idx)
+        {
+            return visit_impl<sizeof...(Ts)>::visit(tup, idx);
+        }
+
+        template <size_t I>
+        struct replace_impl
+        {
+            template <typename T, typename F>
+            static void visit(T& tup, int idx, F value)
+            {
+                if (idx == I - 1){
+                    auto tmp = reinterpret_cast<F&>(std::get<I - 1>(tup));
+                    tmp = reinterpret_cast<F>(value);
+                } else {
+                    replace_impl<I - 1>::visit(tup, idx, value);
+                }
+            }
+        };
+
+        template <>
+        struct replace_impl<0>
+        {
+            template <typename T, typename F>
+            static void visit(T& tup, int idx, F value) { assert(false); }
+        };
+
+        template <typename F, typename... Ts>
+        void replace_at(std::tuple<Ts...> const& tup, int idx, F value)
+        {
+            replace_impl<sizeof...(Ts)>::visit(tup, idx, value);
+        }
+
+        template <typename F, typename... Ts>
+        void replace_at(std::tuple<Ts...>& tup, int idx, F value)
+        {
+            replace_impl<sizeof...(Ts)>::visit(tup, idx, value);
+        }
+
+    };
+
 
     template<typename... components_types>
     struct Entity {
@@ -78,16 +182,50 @@ namespace Ecs
     public:
         Manager() {
             EntityType test1;
-            if (std::get<0>(test1.components) == std::nullopt) {
-                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
-            } else {
-                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
-            }
 
             Component1 first;
             Component2 second;
             Component3 third;
             std::get<0>(test1.components) = std::make_optional(first);
+
+            Tuple<components_types...> tuple;
+            if (tuple.template get<Component1>() == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+            if (tuple.template get<Component2>() == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+            if (tuple.template get<Component2>() == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+
+//            tuple.template set<Component2>(second);
+
+//            std::get<1>(tuple.components) = std::make_optional(second);
+
+            if (tuple.template get<Component1>() == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+            if (tuple.template get<Component2>() == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+            if (tuple.template get<Component3>() == std::nullopt) {
+                std::cout << "Wesh ia R! les std optional marchent!" << std::endl;
+            } else {
+                std::cout << "Ia un problème! il devrait pas y avoir qqch!" << std::endl;
+            }
+
+//            Components::template getElemAt<0>(test1.components) = std::make_optional(first);
 
             // Testing possibilty to pass variable to std::get at compile time using constexpr...
             // Seems to be fucking difficult... That was not expected. Currently working on this
@@ -158,7 +296,7 @@ namespace Ecs
         // TODO: patch this function; actually, std::get can't take runtime variables :()
         template<typename T>
         std::optional<T> getComponent(const EntityType &entity) {
-            return std::get<Components::template getIndex<T>()>(entity.components);
+//            return std::get<Components::template getIndex<T>()>(entity.components);
         }
 
         // TODO: définir une manière d'ajouter des entitées
