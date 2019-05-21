b48dca2025c856e75b1f14d4587af5ccf9df8a4c
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-05-21 17:23:23 +0200

    feat: add unit tests

--- Makefile
+++ Makefile
@@ -12,7 +12,12 @@ all:
 			cd build && cmake .. && cmake --build . -- --no-print-directory
 
 tests_run:
+			if [ ! -d  build ]; then mkdir build; fi
+			cd build && cmake .. -DBUILD_TESTS=ON && cmake --build . -- --no-print-directory && make test ARGS="-V"
+
 coverage:
+			gcovr -r . --exclude-directories tests | tee coverage-litteral.txt
+			gcovr -r . --exclude-directories tests --xml-pretty > coverage.xml
 
 clean:
 			rm -Rf build
--- src/Bootstraper.cpp
+++ src/Bootstraper.cpp
@@ -14,37 +14,10 @@
 
 namespace IndieStudio {
 
-    struct MyComponent {
-        int a;
-        bool b;
-        float c;
-        std::string d;
-    };
-
-    #define O(c, x) (offsetof(c, x))
-
-    auto MyComponentSignature =
-        Signature
-            ::of<MyComponent>
-            ::with<int, bool, float, std::string>
-            ::at({O(MyComponent, a), O(MyComponent, b), O(MyComponent, c), O(MyComponent, d)});
-
     int Bootstraper::start(int ac, char **av)
     {
         (void) ac;
         (void) av;
-
-        std::cout << std::endl << "== TEST SIGNATURE ==" << std::endl;
-        MyComponent cpnt = { 3, true, 3.5, "hello world" };
-        ByteBuffer a;
-        MyComponentSignature.pack(cpnt, a);
-        std::cout << "buffer length: " << a.getSize() << std::endl;
-        MyComponent cpnt2 = { 0, 0, 0, "" };
-        MyComponentSignature.unpack(cpnt2, a);
-        std::cout << cpnt2.a << std::endl;
-        std::cout << cpnt2.b << std::endl;
-        std::cout << cpnt2.c << std::endl;
-        std::cout << cpnt2.d << std::endl;
 
         std::cout << std::endl << "== TEST WORLD ==" << std::endl;
         WorldManager worldManager;
