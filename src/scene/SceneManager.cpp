e400e00c88a2b13d82b39def6b0eeaef2ff0bd94
Juel <juel.marques-garcia@epitech.eu>
2019-06-01 15:03:55 +0200

    feat: add SceneManager::subscribeEvent method

--- src/scene/SceneManager.cpp
+++ src/scene/SceneManager.cpp
@@ -59,8 +59,17 @@ namespace IndieStudio {
         return this->container[key];
     }
 
+    void SceneManager::subscribeEvent(std::function<bool(const irr::SEvent &)> onEvent)
+    {
+        if (this->container.find(this->active) == this->container.end())
+            throw std::runtime_error("subscribeEvent: Failed to get actve scene");
+        this->container[this->active].onEvent = onEvent;
+    }
+
     void SceneManager::setActiveScene(const std::string &key)
     {
+        if (this->container.find(key) == this->container.end())
+            throw std::runtime_error("setAFailed to create scene");
         this->active = key;
     }
 
