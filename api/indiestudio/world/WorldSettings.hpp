cc1b23e1520ec7853e2e8a03214efda747aebed2
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-06-03 20:29:41 +0200

    feat: a lot of work on the new world scene, add a background music to the main menu

--- api/indiestudio/world/WorldSettings.hpp
+++ api/indiestudio/world/WorldSettings.hpp
@@ -7,17 +7,33 @@
 
 #pragma once
 
+#include <irrlicht/irrlicht.h>
 #include "indiestudio/common/ISerializable.hpp"
 
 namespace IndieStudio {
 
     class WorldSettings : public ISerializable {
     public:
+        struct Player {
+            enum class ControlType {
+                KEYBOARD,
+                CONTROLLER,
+                AI
+            };
+
+            ControlType controlType = ControlType::KEYBOARD;
+            irr::EKEY_CODE keyboardUp = irr::KEY_KEY_Z;
+            irr::EKEY_CODE keyboardLeft = irr::KEY_KEY_Q;
+            irr::EKEY_CODE keyboardDown = irr::KEY_KEY_S;
+            irr::EKEY_CODE keyboardRight = irr::KEY_KEY_D;
+            irr::EKEY_CODE keyboardDrop = irr::KEY_KEY_C;
+        };
+
         std::string name;
         unsigned int width = 17;
         unsigned int height = 11;
         std::string worldGenerator = "Basic";
-        unsigned char players = 4;
+        Player players[4];
 
         /* ISerializable implementation */
         void pack(ByteBuffer &buffer) const override;
