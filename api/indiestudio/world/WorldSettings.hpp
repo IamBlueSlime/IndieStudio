c2035ccc3280743bbfb600e178945686e7880287
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-06-04 14:10:15 +0200

    feat: set one player by default and 3 AIs

--- api/indiestudio/world/WorldSettings.hpp
+++ api/indiestudio/world/WorldSettings.hpp
@@ -22,13 +22,33 @@ namespace IndieStudio {
                 AI
             };
 
-            ControlType controlType = ControlType::KEYBOARD;
+            ControlType controlType = ControlType::AI;
             irr::EKEY_CODE keyboardUp = irr::KEY_KEY_Z;
-            irr::EKEY_CODE keyboardLeft = irr::KEY_KEY_Q;
-            irr::EKEY_CODE keyboardDown = irr::KEY_KEY_S;
-            irr::EKEY_CODE keyboardRight = irr::KEY_KEY_D;
-            irr::EKEY_CODE keyboardDrop = irr::KEY_KEY_C;
+            irr::EKEY_CODE keyboardLeft;
+            irr::EKEY_CODE keyboardDown;
+            irr::EKEY_CODE keyboardRight;
+            irr::EKEY_CODE keyboardDrop;
         };
+
+        WorldSettings()
+        {
+            irr::EKEY_CODE keys[4][5] = {
+                { irr::KEY_KEY_Z, irr::KEY_KEY_Q, irr::KEY_KEY_S, irr::KEY_KEY_D, irr::KEY_KEY_E },
+                { irr::KEY_KEY_T, irr::KEY_KEY_F, irr::KEY_KEY_G, irr::KEY_KEY_H, irr::KEY_KEY_Y },
+                { irr::KEY_KEY_I, irr::KEY_KEY_J, irr::KEY_KEY_K, irr::KEY_KEY_L, irr::KEY_KEY_O },
+                { irr::KEY_UP, irr::KEY_LEFT, irr::KEY_DOWN, irr::KEY_RIGHT, irr::KEY_RSHIFT }
+            };
+
+            for (int i = 0; i < 4; i += 1) {
+                this->players[i].keyboardUp = keys[i][0];
+                this->players[i].keyboardLeft = keys[i][1];
+                this->players[i].keyboardDown = keys[i][2];
+                this->players[i].keyboardRight = keys[i][3];
+                this->players[i].keyboardDrop = keys[i][4];
+            }
+
+            this->players[0].controlType = Player::ControlType::KEYBOARD;
+        }
 
         std::string name;
         unsigned short width = 17;
